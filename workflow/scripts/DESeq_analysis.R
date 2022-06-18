b481426864e667e8c035855e2cc587e1436842c0
Anne-Maud Ferreira <ferreira@seneca.embl.de>
2022-06-18 02:05:21 +0200

    Add metadata for samples Commit R script

---
+++ workflow/scripts/DESeq_analysis.R
@@ -0,0 +1,61 @@
+# DEBUG SCRIPT
+# save.image("deseq_analysis.rda")
+# stop()
+
+# Load libraries:
+library(tidyverse) 
+library(DESeq2)
+library(tximport)
+library(rtracklayer)
+
+# Data analysis 
+
+# Gene ID data frame
+tx_g_ids <- read.table("/g/steinmetz/project/dcm_lgreads/mouse_bulkRNA/resources/mus_musculus/transcripts_to_genes.txt")
+tx_g_ids <- tx_g_ids[,c(1,3)]
+colnames(tx_g_ids) <- c("TXNAME", "GENEID")
+# unique(tx_g_ids)
+# Metadata
+# Read samples metadata file
+samples_metadata <- read.table(snakemake@input$meta, header = TRUE)
+
+# Import data from Kallisto alignment
+# Files list
+files <- file.path(samples_metadata$results_dir, 
+                   samples_metadata$mutant, 
+                   samples_metadata$run, 
+                   "abundance.tsv")
+names(files) <- samples_metadata$run
+# Import
+all_txi <- tximport(files, 
+                    type = "kallisto", 
+                    tx2gene = unique(tx_g_ids))
+# ----------------------------------------------------------------------------
+# # 2829 missing transcripts from tx2gene
+# checktx <- read.table(files[1], sep = "\t", header = TRUE)
+# intx2gene <- checktx$target_id %in% tx_g_ids$TXNAME
+# notintx2gene <- which(!intx2gene)
+# txnotintx2gene <- checktx$target_id[notintx2gene]
+# length(txnotintx2gene) # 2829
+# # Remove version
+# first_word <- function(mystring){
+#           unlist(strsplit(mystring, "[.]"))[1]
+# }
+# txnotintx2genenotv <- sapply(txnotintx2gene, first_word)
+# # Download gtf file
+# gtf_file <- import("/g/steinmetz/project/dcm_lgreads/mouse_bulkRNA/resources/mus_musculus/Mus_musculus.GRCm38.96.gtf", 
+#        format = "gtf")
+# all(txnotintx2genenotv %in% mcols(gtf_file)$transcript_id) #not all of them are in the gtf file
+# gtf_txnotintx2genenotv <- dplyr::filter(as.data.frame(mcols(gtf_file)), 
+#                                         transcript_id %in% txnotintx2genenotv)
+# unique(gtf_txnotintx2genenotv$gene_name)
+# ----------------------------------------------------------------------------
+# Number of reads
+# head(all_txi$counts)
+total_nb_reads <- colSums(all_txi$counts)
+total_nb_reads <- rownames_to_column(as.data.frame(total_nb_reads))
+colnames(total_nb_reads)[1] <- c("run")
+total_nb_reads_merge_w_metadata <- left_join(samples_metadata, total_nb_reads)
+write.csv(total_nb_reads_merge_w_metadata[,c("mutant", "run", "genotype", "total_nb_reads")], 
+          file = "/g/steinmetz/project/dcm_lgreads/mouse_bulkRNA/results/total_nb_reads.csv", 
+          col.names = TRUE, row.names = FALSE)
