220231e23d5011334b086cf70dcd4d7d786b14f4
Anne-Maud Ferreira <ferreira@seneca.embl.de>
2022-08-03 21:41:53 +0200

    Heatmaps

--- workflow/scripts/DESeq_analysis_withSTARalignment.R
+++ workflow/scripts/DESeq_analysis_withSTARalignment.R
@@ -26,9 +26,9 @@ sample_table <- read.table("/g/steinmetz/project/dcm_lgreads/mouse_bulkRNA/confi
 # read.table(snakemake@input$meta, header = TRUE)
 # Simplify sample table
 sample_table$sample <- paste("results/STAR", 
-                                  sample_table$mutant, 
-                                  sample_table$run, 
-                                  "Aligned.sortedByCoord.out.bam", sep = "/")
+                             sample_table$mutant, 
+                             sample_table$run, 
+                             "Aligned.sortedByCoord.out.bam", sep = "/")
 
 # PCA
 # Prepare data for PCA
@@ -70,8 +70,8 @@ if(all(colnames(featurecounts_P635L) == sample_table_P635L$sample)){
 }
 # Create deseq2 object
 dds_P635L <- DESeqDataSetFromMatrix(countData = featurecounts_P635L,
-                              colData = sample_table_P635L[,c("run", "genotype")],
-                              design = ~ genotype)
+                                    colData = sample_table_P635L[,c("run", "genotype")],
+                                    design = ~ genotype)
 # Perform DE test
 dds_P635L <- DESeq(dds_P635L)
 # P635L hom vs WT
@@ -126,14 +126,14 @@ if(all(colnames(featurecounts_wt) == sample_table_wt$sample)){
 }
 # Create deseq2 object
 dds_wt <- DESeqDataSetFromMatrix(countData = featurecounts_wt,
-                                    colData = sample_table_wt[,c("mutant", "run")],
-                                    design = ~ mutant)
+                                 colData = sample_table_wt[,c("mutant", "run")],
+                                 design = ~ mutant)
 # Perform DE test
 dds_wt <- DESeq(dds_wt)
 # P635L vs R636Q wt
 res_P635LvsR636Q_wt <- results(dds_wt, contrast=c("mutant", "P635L", "R636Q"))
 res_P635LvsR636Q_wt_sig <- dplyr::filter(as.data.frame(res_P635LvsR636Q_wt), 
-                                       padj < 0.05)
+                                         padj < 0.05)
 res_P635LvsR636Q_wt_sig <- rownames_to_column(res_P635LvsR636Q_wt_sig, var = "gene")
 write.table(x = res_P635LvsR636Q_wt_sig,
             file = "results/table_STARalignment_res_P635LvsR636Q_wt_sig.txt",
@@ -160,7 +160,7 @@ function_my_heatmap <- function(mutation, dds, res, sample_tab, annot, filter_co
   # Extract top genes
   counts_allsamples <- counts(dds, normalized=TRUE)[res_ordered[1:top, "gene"],]
   counts_allsamples <- rownames_to_column(as.data.frame(counts_allsamples), 
-                                                            var = "gene")
+                                          var = "gene")
   # Select genotype samples
   counts_sig <- dplyr::select(counts_allsamples, c(df_table$run, "gene"))
   # Add gene name
@@ -207,30 +207,200 @@ function_my_heatmap(mutation = "WT",
                     top = 20)
 dev.off()
 
-# # Order genes based on adjusted p-values
-# res_R636Q_homvswt_sig_ordered <- res_R636Q_homvswt_sig[order(res_R636Q_homvswt_sig$padj),]
-# # Select hom and wt samples
-# df_hom_wt_R636Q_table <- dplyr::filter(sample_table_R636Q, genotype == "hom" | genotype == "wt") %>% 
-#   select(run, genotype)
-# row.names(df_hom_wt_R636Q_table) <- df_hom_wt_R636Q_table$run
-# # Extract top 20 genes
-# counts_R636Q_homvswt_sig_allsamples <- counts(dds_R636Q, normalized=TRUE)[res_R636Q_homvswt_sig_ordered[1:20,"gene"],]
-# counts_R636Q_homvswt_sig_allsamples <- rownames_to_column(as.data.frame(counts_R636Q_homvswt_sig_allsamples), 
-#                                                var = "gene")
-# # Select hom and wt samples
-# counts_R636Q_homvswt_sig <- dplyr::select(counts_R636Q_homvswt_sig_allsamples, 
-#                                           c(df_hom_wt_R636Q_table$run, "gene"))
-# # Add gene name
-# counts_R636Q_homvswt_sig_wgname <- left_join(counts_R636Q_homvswt_sig, 
-#                                              gen_vM29_annot_gene_uniq)
-# counts_R636Q_homvswt_sig_wgname <- column_to_rownames(counts_R636Q_homvswt_sig_wgname, "gene_name")
-# # Plot
-# pdf("results/heatmap_R636Q_homvswt_top20.pdf")
-# pheatmap(counts_R636Q_homvswt_sig_wgname[,1:length(df_hom_wt_R636Q_table$run)], 
-#          cluster_rows = TRUE, 
-#          show_rownames = TRUE,
-#          show_colnames = FALSE,
-#          cluster_cols = TRUE,
-#          annotation_col = df_hom_wt_R636Q_table,
-#          scale = "row")
-# dev.off()
+# Compute fold change per individuals for each mutation
+# log2(hom/wt)
+# P635L
+dim(counts(dds_P635L, normalized = TRUE))
+geneexp_dds_P635L <- t(counts(dds_P635L, normalized = TRUE))
+# Average per gene for WT
+wt_IDs <- dplyr::filter(sample_table_P635L, genotype == "wt") %>% pull(run)
+pergene_P635L_WT <- dplyr::select(as.data.frame(counts(dds_P635L, , normalized = TRUE)), wt_IDs)
+avg_pergene_P635L_WT <- rowMeans(pergene_P635L_WT)
+# Remove WT columns
+geneexp_dds_P635L_hom_het <- dplyr::select(as.data.frame(counts(dds_P635L, normalized = TRUE)), -wt_IDs)
+# Long format
+geneexp_dds_P635L_hom_het <- rownames_to_column(.data = geneexp_dds_P635L_hom_het, 
+                                                var = "gene")
+geneexp_dds_P635L_hom_het_lg <- pivot_longer(geneexp_dds_P635L_hom_het, 
+                                             cols = !gene, 
+                                             names_to = "run")
+# Join genotype
+geneexp_dds_P635L_hom_het_lg_wgenot <- left_join(geneexp_dds_P635L_hom_het_lg, 
+                                                 sample_table_P635L[, c("mutant", "run", "genotype")])
+# Join wt average value
+avg_pergene_P635L_WT_df <- data.frame(gene=names(avg_pergene_P635L_WT), 
+                                      wt_avg=avg_pergene_P635L_WT, 
+                                      row.names=NULL)
+geneexp_dds_P635L_hom_het_lg_wgenot_wwt <- left_join(geneexp_dds_P635L_hom_het_lg_wgenot, 
+                                                     avg_pergene_P635L_WT_df)
+# Filter genes with zero reads in WT mice as I won't be able to compute a log2 FC for these genes
+geneexp_dds_P635L_hom_het_lg_wgenot_wwt <- dplyr::filter(geneexp_dds_P635L_hom_het_lg_wgenot_wwt,
+                                                         wt_avg != 0)
+# FC
+geneexp_dds_P635L_hom_het_lg_log2FC <- geneexp_dds_P635L_hom_het_lg_wgenot_wwt %>% 
+  group_by(gene, run) %>% 
+  mutate(log2_FC = log2(value/wt_avg))
+# Replace -Inf by NA
+# dplyr::filter(geneexp_dds_P635L_hom_het_lg_log2FC, log2_FC == -Inf)
+geneexp_dds_P635L_hom_het_lg_log2FC <- mutate(geneexp_dds_P635L_hom_het_lg_log2FC, 
+                                              log2_FC = replace(log2_FC, log2_FC == -Inf, NA))
+
+# R636Q
+dim(counts(dds_R636Q, normalized = TRUE))
+geneexp_dds_R636Q <- t(counts(dds_R636Q, normalized = TRUE))
+# Average per gene for WT
+wt_IDs <- dplyr::filter(sample_table_R636Q, genotype == "wt") %>% pull(run)
+pergene_R636Q_WT <- dplyr::select(as.data.frame(counts(dds_R636Q, normalized = TRUE)), wt_IDs)
+avg_pergene_R636Q_WT <- rowMeans(pergene_R636Q_WT)
+# Remove WT columns
+geneexp_dds_R636Q_hom_het <- dplyr::select(as.data.frame(counts(dds_R636Q, normalized = TRUE)), -wt_IDs)
+# Long format
+geneexp_dds_R636Q_hom_het <- rownames_to_column(.data = geneexp_dds_R636Q_hom_het, 
+                                                var = "gene")
+geneexp_dds_R636Q_hom_het_lg <- pivot_longer(geneexp_dds_R636Q_hom_het, 
+                                             cols = !gene, 
+                                             names_to = "run")
+# Join genotype
+geneexp_dds_R636Q_hom_het_lg_wgenot <- left_join(geneexp_dds_R636Q_hom_het_lg, 
+                                                 sample_table_R636Q[, c("mutant", "run", "genotype")])
+# Join wt average value
+avg_pergene_R636Q_WT_df <- data.frame(gene=names(avg_pergene_R636Q_WT), 
+                                      wt_avg=avg_pergene_R636Q_WT, 
+                                      row.names=NULL)
+geneexp_dds_R636Q_hom_het_lg_wgenot_wwt <- left_join(geneexp_dds_R636Q_hom_het_lg_wgenot, 
+                                                     avg_pergene_R636Q_WT_df)
+# Filter genes with zero reads in WT mice as I won't be able to compute a log2 FC for these genes
+geneexp_dds_R636Q_hom_het_lg_wgenot_wwt <- dplyr::filter(geneexp_dds_R636Q_hom_het_lg_wgenot_wwt,
+                                                         wt_avg != 0)
+# FC
+geneexp_dds_R636Q_hom_het_lg_log2FC <- geneexp_dds_R636Q_hom_het_lg_wgenot_wwt %>% 
+  group_by(gene, run) %>% 
+  mutate(log2_FC = log2(value/wt_avg))
+# Replace -Inf by NA
+# dplyr::filter(geneexp_dds_R636Q_hom_het_lg_log2FC, log2_FC == -Inf)
+geneexp_dds_R636Q_hom_het_lg_log2FC <- mutate(geneexp_dds_R636Q_hom_het_lg_log2FC, 
+                                              log2_FC = replace(log2_FC, log2_FC == -Inf, NA))
+
+# Heatmap
+# Filter gene list based on Markus list
+DEgenelist_p_inf_1e4 <- read.table("results/DEgenelist_p_inf_1e-4.txt")
+DEgenelist_p_inf_1e6 <- read.table("results/DEgenelist_p_inf_1e-6.txt")
+# Merge with ensembl IDs
+DEgenelist_p_inf_1e4_wname <- left_join(data.frame("gene_name" = DEgenelist_p_inf_1e4[,1]), 
+                                        gen_vM29_annot_gene_uniq)
+DEgenelist_p_inf_1e6_wname <- left_join(data.frame("gene_name" = DEgenelist_p_inf_1e6[,1]), 
+                                        gen_vM29_annot_gene_uniq)
+# Merge gene name with fold change and order genes according to the avg log2 FC
+fct_merge_FC_wgenename_and_order <- function(geneexp, DEgenelist){
+  # Merge gene name with fold change
+  df_merge <- left_join(geneexp, DEgenelist)
+  # Filter NA row 
+  df_filtered <- dplyr::filter(df_merge, 
+                               run != "NA")
+  # Order per log2FC average
+  df_filtered_avglg2fc <- df_filtered %>% 
+    group_by(gene) %>% 
+    mutate(avg_log2FC = mean(log2_FC, na.rm = TRUE))
+  df_filtered_avglg2fc[order(df_filtered_avglg2fc$avg_log2FC, decreasing = TRUE),]
+}
+# P635L p < 1e-4
+heatmap_data_P635L_hom_het_p_inf_1e4 <- fct_merge_FC_wgenename_and_order(
+  geneexp = DEgenelist_p_inf_1e4_wname,
+  DEgenelist = geneexp_dds_P635L_hom_het_lg_log2FC
+)
+# P635L p < 1e-6
+heatmap_data_P635L_hom_het_p_inf_1e6 <- fct_merge_FC_wgenename_and_order(
+  geneexp = DEgenelist_p_inf_1e6_wname,
+  DEgenelist = geneexp_dds_P635L_hom_het_lg_log2FC
+)
+# R636Q p < 1e-4
+heatmap_data_R636Q_hom_het_p_inf_1e4 <- fct_merge_FC_wgenename_and_order(
+  geneexp = DEgenelist_p_inf_1e4_wname,
+  DEgenelist = geneexp_dds_R636Q_hom_het_lg_log2FC
+)
+# R636Q p < 1e-6
+heatmap_data_R636Q_hom_het_p_inf_1e6 <- fct_merge_FC_wgenename_and_order(
+  geneexp = DEgenelist_p_inf_1e6_wname,
+  DEgenelist = geneexp_dds_R636Q_hom_het_lg_log2FC
+)
+
+# Transform to wide format + Collapse run with genotype
+fct_wide_gen_order <- function(heatmapdata){
+  # Wide format
+  df_w <- pivot_wider(heatmapdata, 
+                      id_cols = c("run", "genotype"),
+                      names_from = "gene_name", 
+                      values_from = "log2_FC")
+  # Collapse run with genotype
+  df_w_gen <- df_w %>% 
+    rowwise() %>% 
+    mutate(sample = paste(run, genotype, sep = "_"), .before = run)
+  df_w_gen <- dplyr::select(df_w_gen, -c(run, genotype))
+  column_to_rownames(df_w_gen, var = "sample")
+}
+# P635L p < 1e-4
+heatmap_data_P635L_hom_het_p_inf_1e4_w <- fct_wide_gen_order(
+  heatmapdata = heatmap_data_P635L_hom_het_p_inf_1e4
+)
+# P635L p < 1e-6
+heatmap_data_P635L_hom_het_p_inf_1e6_w <- fct_wide_gen_order(
+  heatmapdata = heatmap_data_P635L_hom_het_p_inf_1e6
+)
+# R636Q p < 1e-4
+heatmap_data_R636Q_hom_het_p_inf_1e4_w <- fct_wide_gen_order(
+  heatmapdata = heatmap_data_R636Q_hom_het_p_inf_1e4
+)
+# R636Q p < 1e-6
+heatmap_data_R636Q_hom_het_p_inf_1e6_w <- fct_wide_gen_order(
+  heatmapdata = heatmap_data_R636Q_hom_het_p_inf_1e6
+)
+
+# Heatmaps
+png("results/heatmap_STARalignment_P635L_hom_het_p_inf_1e4.png", 
+    width = 8, height = 20, units = "in", res = 1000)
+pheatmap(t(heatmap_data_P635L_hom_het_p_inf_1e4_w),
+         na_col = "black",
+         cluster_rows = FALSE, 
+         show_rownames = TRUE,
+         show_colnames = TRUE,
+         cluster_cols = FALSE,
+         fontsize_row = 2, 
+         main = "P635L with DE genes p < 1e-4")
+dev.off()
+png("results/heatmap_STARalignment_P635L_hom_het_p_inf_1e6.png", 
+    width = 8, height = 20, units = "in", res = 1000)
+pheatmap(t(heatmap_data_P635L_hom_het_p_inf_1e6_w),
+         na_col = "black",
+         cluster_rows = FALSE, 
+         show_rownames = TRUE,
+         show_colnames = TRUE,
+         cluster_cols = FALSE,
+         fontsize_row = 2, 
+         main = "P635L with DE genes p < 1e-6")
+dev.off()
+# Order heatmap rows
+idx_het <- grep("*het$", rownames(heatmap_data_R636Q_hom_het_p_inf_1e4_w))
+idx_hom <- grep("*hom$", rownames(heatmap_data_R636Q_hom_het_p_inf_1e4_w))
+idx <- c(idx_hom, idx_het)
+png("results/heatmap_STARalignment_R636Q_hom_het_p_inf_1e4.png", 
+    width = 8, height = 20, units = "in", res = 1000)
+pheatmap(t(heatmap_data_R636Q_hom_het_p_inf_1e4_w[idx,]),
+         na_col = "black",
+         cluster_rows = FALSE, 
+         show_rownames = TRUE,
+         show_colnames = TRUE,
+         cluster_cols = FALSE,
+         fontsize_row = 2, 
+         main = "R636Q with DE genes p < 1e-4")
+dev.off()
+png("results/heatmap_STARalignment_R636Q_hom_het_p_inf_1e6.png", 
+    width = 8, height = 20, units = "in", res = 1000)
+pheatmap(t(heatmap_data_R636Q_hom_het_p_inf_1e6_w[idx,]),
+         na_col = "grey90",
+         cluster_rows = FALSE, 
+         show_rownames = TRUE,
+         show_colnames = TRUE,
+         cluster_cols = FALSE,
+         fontsize_row = 2, 
+         main = "R636Q with DE genes p < 1e-6")
+dev.off()
