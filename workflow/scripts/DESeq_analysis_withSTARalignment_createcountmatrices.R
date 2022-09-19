139034e460daf6dc4236076f3c80802283a5eb12
Anne-Maud Ferreira <ferreira@seneca.embl.de>
2022-09-19 19:58:41 +0200

    Modify the script to accommodate the new rule to create one count matrix for DESeq analysis with star alignment per experiment

--- workflow/scripts/DESeq_analysis_withSTARalignment_createcountmatrices.R
+++ workflow/scripts/DESeq_analysis_withSTARalignment_createcountmatrices.R
@@ -10,15 +10,17 @@ library(Rsubread)
 #                          pattern="Aligned.sortedByCoord.out.bam$", 
 #                          recursive = TRUE,
 #                          full.names = TRUE)
-star_files <- c(snakemake@input$P635L_alignment_files, snakemake@input$R636Q_alignment_files)
+# star_files <- c(snakemake@input$P635L_alignment_files, 
+#                 snakemake@input$R636Q_alignment_files)
+star_files <- unlist(snakemake@input)
 
 # Create counts from STAR alignment
 # featureCounts() from the Rsubread package
 featurecounts_allsamples <- featureCounts(file = star_files,
                                           annot.ext = "resources/gencode.vM29.annotation.gtf",
                                           isGTFAnnotationFile = TRUE,
-                                          strandSpecific = 2,
+                                          strandSpecific = snakemake@params$strandness,
                                           isPairedEnd = TRUE,
                                           nthreads = snakemake@threads)
 # Save object
-save(featurecounts_allsamples, file = paste0("results/robject_featurecounts_allsamples.rda"))
+saveRDS(featurecounts_allsamples, file = unlist(snakemake@output))
